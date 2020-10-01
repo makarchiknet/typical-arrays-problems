bf559116ef8be79741ef9769e4587b6a6ba58df1
Kate Makarova <makarchiknet@gmail.com>
2020-10-02 00:46:51 +0300

    init: start typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min(array) {
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  return Math.min(...array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  return Math.max(...array);
 
-exports.avg = function avg (array) {
-  return 0;
-}
+};
+
+exports.avg = function avg(array) {
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  const sum = array.reduce((partialSum, a) => partialSum + a, 0);
+  return sum / array.length;
+};
